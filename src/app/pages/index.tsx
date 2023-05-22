1c555e8281184ee780ae0e6b4000569d86640288
wmazurwat <wojciech.mazur.lo@gmail.com>
2023-05-22 20:51:55 +0200

    update

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,48 @@
+// pages/index.js
+
+import allProducts from "./data/products.json";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+export default function Home({ allProducts }) {
+  return (
+    <>
+      <Head>
+        <title>Plants | Home</title>
+      </Head>
+      <div className="container">
+        <h2 className={styles.title}>
+          All Products <span>🌿</span>
+        </h2>
+        <div className={styles.products_container}>
+          {allProducts.map((product) => {
+            return (
+              <div className={styles.product_card} key={product.id}>
+                <Link href={`products/${product.slug}`}>
+                  <a>
+                    <div className={styles.product_img}>
+                      <img src={product.image.url} alt={product.name} />
+                    </div>
+                  </a>
+                </Link>
+                <div className={styles.product_content}>
+                  <h3>{product.name}</h3>
+                  <p>${product.price}</p>
+                  <button className="btn">Add to cart 🛒</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      allProducts,
+    },
+  };
+}
